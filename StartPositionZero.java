4a2ed74ab3dcecd15490b181632139e186649b0f
unknown <gayoyawalkar@gmail.com>
2021-09-29 12:56:12 +0530

    Added:PositionZero Uc1

---
+++ StartPositionZero.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.snakeladder;
+//Uc1
+public class StartPositionZero {
+	public static int startWithZeroPosition() {
+		int position=0;
+		return position;
+	}
+	public static void main(String[] args) {
+		int player1Pos=startWithZeroPosition();
+}
+}
